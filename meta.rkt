960c94e31912ab93227a11fa6cdcee1b96734869
Sam Frances <sam@samfrances.uk>
2021-07-16 18:57:05 +0100

    Bookmark: if-expressions

--- meta.rkt
+++ meta.rkt
@@ -1,3 +1,5 @@
+#lang racket
+
 ; Metacircular evaluator
 ;
 ; In this file, we will implement the meta-circular evaluator as described
@@ -18,13 +20,69 @@
 ; Big idea:  All of computation is expressed by combinations of eval and apply.
 ; Thus, these two procedures are what we'll work with.  
 
-(define (seval ...)
-    ...
+;; Environments
 
+(define (make-environment)
+  (make-hash))
+
+(define (lookup-environment environ name)
+  (hash-ref environ name))
+
+(define (define-in-environment environ name value)
+  (hash-set! environ name value))
+
+(define globalenv (make-environment))
+(define-in-environment globalenv '+ +)
+(define-in-environment globalenv '* *)
+(define-in-environment globalenv '/ /)
+(define-in-environment globalenv '- -)
+(define-in-environment globalenv '= =)
+(define-in-environment globalenv 'x 5)
+
+
+
+;; Eval/Apply
+
+(define (seval exp environ)
+    (cond [(primitive? exp) exp]
+          [(symbol? exp)
+           (lookup-environment environ exp)]
+          [(if-exp? exp)
+           (seval-if-statement exp environ)]
+          [(list? exp)
+           (sapply (car exp) (cdr exp) environ)]
+          [else (error "Bad expression" exp)])
 )
 
-(define (sapply ...)
-    ...
-)
+(define (sapply proc arguments environ)
+    (let [(eproc (seval proc environ))
+          (earguments (map
+                       (λ (arg) (seval arg environ))
+                       arguments))]
+      (apply eproc earguments)))
 
+
+(define (primitive? exp)
+  (or (number? exp)
+      (boolean? exp)))
+
+;; "If" special form
+(define (if-exp? exp)
+  (and (list? exp)
+       (= (length exp) 4)
+       (eq? (car exp) 'if)))
+
+(define (test if-exp)
+  (cadr if-exp))
+
+(define (true-branch if-exp)
+  (caddr if-exp))
+
+(define (else-branch if-exp)
+  (cadddr if-exp))
+
+(define (seval-if-statement if-exp environ)
+    (if (seval (test if-exp) environ)
+        (seval (true-branch if-exp) environ)
+        (seval (else-branch if-exp) environ)))
 
