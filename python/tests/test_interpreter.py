c3d567145d4fbbf5e677f8122496d2e411ddb184
Sam Frances <sam@samfrances.uk>
2024-05-04 16:07:29 +0100

    Test nested addition

--- python/tests/test_interpreter.py
+++ python/tests/test_interpreter.py
@@ -1,6 +1,12 @@
 from ..interpreter import seval, create_global_env
 
-def test_one():
+def test_simple_addition():
     env = create_global_env()
     ast = ["+", 1, 2]
     assert seval(ast, env) == 3
+
+
+def test_nested_addition():
+    env = create_global_env()
+    ast = ["+", ["+", 1, 2], 7]
+    assert seval(ast, env) == 10
